a6433164d82bae36814254cda9b1b4beca06fab5
Darío Romero Romera <darioromeroromera@gmail.com>
2023-10-16 16:49:41 +0200

    Terminado ejercicio 4

---
+++ app/src/main/java/com/example/ejemplo1/OtroActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ejemplo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otro)
+    }
+}
